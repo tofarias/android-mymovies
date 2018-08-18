dcb916eac9d1881b3b5faf829e0d80dcc884170f
Tiago Farias <tiago.farias.poa@gmail.com>
2018-08-18 18:35:15 -0300

    Add criando radio buttons dinâmicamente

--- app/src/main/java/com/example/tiago/mymovies/RegisterActivity.java
+++ app/src/main/java/com/example/tiago/mymovies/RegisterActivity.java
@@ -2,14 +2,21 @@ package com.example.tiago.mymovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatRadioButton;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.tiago.mymovies.dao.MovieDao;
+import com.example.tiago.mymovies.dao.db.CategoryDaoDb;
 import com.example.tiago.mymovies.dao.db.MovieDaoDb;
 import com.example.tiago.mymovies.model.Category;
 import com.example.tiago.mymovies.model.Movie;
+
+import java.util.List;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,6 +24,31 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.createCategoryRadioButtonsInsideRadioGroup();
+    }
+
+    public void createCategoryRadioButtonsInsideRadioGroup()
+    {
+        CategoryDaoDb categoryDao = new CategoryDaoDb(this);
+        List<Category> categoryList = categoryDao.listAll();
+
+        AppCompatRadioButton[] rb = new AppCompatRadioButton[categoryList.size()];
+
+        RadioGroup rgp = (RadioGroup) findViewById(R.id.rb_categories);
+        rgp.setOrientation(LinearLayout.VERTICAL);
+
+        for (int i = 0; i < categoryList.size(); i++) {
+            RadioButton rbn = new RadioButton(this);
+            rbn.setId(categoryList.get(i).getId());
+            rbn.setText(categoryList.get(i).getName());
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT, 1f
+            );
+            rbn.setLayoutParams(params);
+            rgp.addView(rbn);
+        }
     }
 
     public void addMovie(View view){
