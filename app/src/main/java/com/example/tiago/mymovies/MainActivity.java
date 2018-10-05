03c036a476cd90a443d83c96973b0615a0656c34
Tiago Farias <tiago.farias.poa@gmail.com>
2018-10-05 20:13:14 -0300

    Add - serviço para executar áudio em segundo plano logo quando a aplicação for iniciada.

--- app/src/main/java/com/example/tiago/mymovies/MainActivity.java
+++ app/src/main/java/com/example/tiago/mymovies/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.tiago.mymovies;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.example.tiago.mymovies.Service.AudioBackgroundService;
 import com.example.tiago.mymovies.adapter.MovieAdapter;
 import com.example.tiago.mymovies.dao.MovieDao;
 import com.example.tiago.mymovies.dao.db.MovieDaoDb;
@@ -37,11 +40,25 @@ public class MainActivity extends AppCompatActivity {
         this.recyclerView.setAdapter(movieAdapter);
         this.recyclerView.addItemDecoration(
                 new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        this.playBackgroundAudio();
     }
 
     public void openForm(View v){
         Intent it = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(it);
+    }
+
+    public  void playBackgroundAudio(){
+
+        Intent intent = new Intent(MainActivity.this , AudioBackgroundService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(MainActivity.this , 0 , intent , PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        if(alarmManager !=null) {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                    + (5 * 1000), pendingIntent);
+        }
+
     }
 
     @Override
